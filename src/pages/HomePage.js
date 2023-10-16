4148de60dbad040e943f5164b9902d4f2dc89ba8
arieftheluffy <arieftheluffy@gmail.com>
2023-10-16 10:35:19 +0800

    update-home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,16 +3,25 @@ import { css } from '@emotion/react';
 
 const HomePageStyle = css`
   h3 {
-    font-size: 5rem;
-    font-weight: 600;
+    font-size: 3rem;
+    font-weight: 500;
     text-align: center;
+    margin: 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
   }
 `;
 
 const HomePage = () => {
   return (
     <div css={[HomePageStyle]}>
-      <h3 className="title">Wellcome to 4Project!</h3>
+      <br></br>
+      <h3 className="title">Home Page nya belum siap...</h3>
     </div>
   );
 };
